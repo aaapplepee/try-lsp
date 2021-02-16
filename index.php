28ea4015b6a178030a64e8546bfedfa16a7c5851
akmalwidad <widad.akmal83@gmail.com>
2021-02-16 08:16:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <!-- Navbar -->
+    <header class="navbar">
+        <div class="nav-brand">
+            <h3>Pengaduan Masyarakat</h3>
+        </div>
+        <div class="nav-list">
+                <a href="#" class="nav-item">Home</a>
+                <a href="#" class="nav-item">Register</a>
+                <a href="#" class="nav-item">Login</a>
+        </div>
+    </header>
+    <!-- End Navbar -->
+
+    <!-- Hero -->
+    <section class="hero-section">
+        <div class="hero-content">
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aspernatur officia odit consectetur earum, nemo veniam et dignissimos dolores aperiam quasi laboriosam dicta sunt id. Dolor perferendis maiores id voluptatum.</h3>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa tenetur quod possimus dolor natus, dolore est delectus provident voluptatum aspernatur corrupti. Omnis iste adipisci quibusdam recusandae, saepe numquam velit consequatur?</p>
+
+            <a href="#" class="button-link">Lapor!</a>
+
+        </div>
+        <div class="hero-image">
+            <img src="assets/image/report.png" alt="" height="100%" width="100%">
+        </div>
+    </section>
+    <!-- End Hero -->
+</body>
+</html>
